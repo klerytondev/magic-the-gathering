f17ae3257b560e8ff3addd31b3224c02f3941608
Kleryton <kleryton.dev@gmail.com>
2022-05-05 08:31:31 -0300

    create project

---
+++ src/test/java/br/com/kleryton/magicthegathering/MagicTheGatheringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kleryton.magicthegathering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicTheGatheringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
